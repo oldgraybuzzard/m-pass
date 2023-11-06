6ef4520f6f356b004ac015f0d69e3860591f7c21
oldgraybuzzard <k_felder@me.com>
2023-11-05 19:30:20 -0500

    server working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,96 @@
+const { User, Password } = require('../models'); 
+
 const resolvers = {
   Query: {
-    getUser: async (_, args, context) => {
-      // Implement logic to fetch a user from your data source based on args
-      // For example, you can use the User model to find a user by their ID or email
-      const user = await User.findById(args.userId);
-      return user;
+    getUserById: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (error) {
+        throw new Error('Failed to fetch user by ID');
+      }
+    },
+    getAllUsers: async () => {
+      try {
+        const users = await User.find();
+        return users;
+      } catch (error) {
+        throw new Error('Failed to fetch all users');
+      }
+    },
+    getPasswordById: async (_, { passwordId }) => {
+      try {
+        const password = await Password.findById(passwordId);
+        return password;
+      } catch (error) {
+        throw new Error('Failed to fetch password by ID');
+      }
+    },
+    getAllPasswords: async () => {
+      try {
+        const passwords = await Password.find();
+        return passwords;
+      } catch (error) {
+        throw new Error('Failed to fetch all passwords');
+      }
     },
   },
-  // Other resolvers for mutations, user authentication, etc.
+  Mutation: {
+    createUser: async (_, { input }) => {
+      try {
+        const user = await User.create(input);
+        return user;
+      } catch (error) {
+        if (error.name === 'ValidationError') {
+          // Handle validation errors (e.g., required fields missing)
+          const validationErrors = Object.values(error.errors).map((error) => error.message);
+          throw new Error(`Validation failed: ${validationErrors.join(', ')}`);
+        }
+        // Handle other errors
+        throw new Error('Failed to create user');
+      }
+    },
+    updateUser: async (_, { userId, input }) => {
+      try {
+        const user = await User.findByIdAndUpdate(userId, input, { new: true });
+        return user;
+      } catch (error) {
+        throw new Error('Failed to update user');
+      }
+    },
+    deleteUser: async (_, { userId }) => {
+      try {
+        const user = await User.findByIdAndDelete(userId);
+        return user;
+      } catch (error) {
+        throw new Error('Failed to delete user');
+      }
+    },
+    createPassword: async (_, { input }) => {
+      try {
+        const password = await Password.create(input);
+        return password;
+      } catch (error) {
+        throw new Error('Failed to create password');
+      }
+    },
+    updatePassword: async (_, { passwordId, input }) => {
+      try {
+        const password = await Password.findByIdAndUpdate(passwordId, input, { new: true });
+        return password;
+      } catch (error) {
+        throw new Error('Failed to update password');
+      }
+    },
+    deletePassword: async (_, { passwordId }) => {
+      try {
+        const password = await Password.findByIdAndDelete(passwordId);
+        return password;
+      } catch (error) {
+        throw new Error('Failed to delete password');
+      }
+    },
+  },
 };
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,21 +2,52 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    id: ID!
+    _id: ID!
     firstName: String!
     lastName: String!
     email: String!
   }
 
+  type Password {
+    _id: ID!
+    name: String!
+    category: String!
+    email: String!
+    username: String!
+    password: String!
+    notes: String
+    createdAt: String!
+  }
+
+  input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+  }
+
+  input PasswordInput {
+    name: String!
+    category: String!
+    email: String!
+    username: String!
+    password: String!
+    notes: String
+  }
+
   type Query {
-    getUser(id: ID!): User
+    getUserById(userId: ID!): User
     getAllUsers: [User]
+    getPasswordById(passwordId: ID!): Password
+    getAllPasswords: [Password]
   }
 
   type Mutation {
-    createUser(firstName: String!, lastName: String!, email: String!): User
-    updateUser(id: ID!, firstName: String, lastName: String, email: String): User
-    deleteUser(id: ID!): User
+    createUser(input: UserInput): User
+    updateUser(userId: ID!, input: UserInput): User
+    deleteUser(userId: ID!): User
+    createPassword(input: PasswordInput): Password
+    updatePassword(passwordId: ID!, input: PasswordInput): Password
+    deletePassword(passwordId: ID!): Password
   }
 `;
 
